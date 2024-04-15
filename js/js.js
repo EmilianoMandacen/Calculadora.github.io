7ca4f0262d734e09a123c548b3a779657745d40b
EmilianoMandacen <146836334+EmilianoMandacen@users.noreply.github.com>
2024-04-15 14:28:19 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -7,9 +7,8 @@ function dividirOctal() {
     if(valor1 != 0 ) {
         let resultado = Math.floor(valor1 / valor2);
         let resto = valor1 % valor2;
-        document.querySelector("#resultado").innerHTML  = "El resultado de la division es: " + resultado + " y el resto es: " + resto ;
+        document.querySelector("#resultado").innerHTML  = "El cociente de la division es: " + resultado + " y el resto es: " + resto ;
         document.querySelector("#valor1").value = resultado;
-
         if (resto == 10){
             resto = "A";
         }
@@ -28,9 +27,12 @@ function dividirOctal() {
         if (resto == 15){
             resto = "F";
         }
-
         let array = [];
         array.push(resto);
         document.querySelector("#resto").innerHTML += " " + array;
+    }
+
+    if (valor1 == 0) {
+        alert("El dividendo no puede ser 0")
     }
 }
